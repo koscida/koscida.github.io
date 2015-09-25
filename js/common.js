aa1dbd3be60dce9d4672c8d234a4452277c001e7
Brittany Ann Kos <britkos@gmail.com>
2015-09-24 23:08:47 -0600

    new styles

--- js/common.js
+++ js/common.js
@@ -1,32 +1,55 @@
-$(document).ready(function() {
+var minheight = 20;
+var maxheight = 100;
+var time = 1000;
+var timer = null;
+var toggled = false;
+
+var doit;
+
+window.onload = function() {
 	
 	//resize screen
-	var doit;
-	$(window).resize(function () {
+	window.addEventListener("resize", function (){
 		clearTimeout(doit);
-		doit = setTimeout(windowResize, 100);
+		doit = setInterval(function(){
+			var windowH = window.innerHeight;
+			var pageH = document.getElementById("page").style.height;
+			if(pageH <= (windowH-150)) {
+				document.getElementById("page").style.height = ( (windowH - 110) + "px");
+			}
+		}, 1);
 	});
-	windowResize();
+	//windowResize();
 	
-	$("#nav_expand").click(function(){
-		$("#navigation").slideToggle();
-	});
+	
+	
 	
 	addContactInfo();
-});
+};
 
 function windowResize() {
-	//alert("hi");
-	var winH = $(window).height();
-	var pageH = $("#page").height();
-	var pageH2 = $("header").height() + $("#content").height();
-	//alert("winH: " + winH + "\npageH: " + pageH + "\npageH2: " + pageH2);
-	if(pageH <= (winH-150))
-		$("#page").height(winH - 150);
+	var windowH = window.innerHeight;
+	var pageH = document.getElementById("page").style.height;
+	if(pageH <= (windowH-150) && window.innerWidth >= 767) {
+			document.getElementById("page").style.height = ( (windowH - 110) + "px");
+	}
 	
 }
 
 function addContactInfo() {
-	$("#brit_email").html("brittany.kos [at] colorado [dot] edu");
-	$("#brit_phone").html("720-270-5003");
+	document.getElementById("brit_email").innerHTML = "brittany.kos [at] colorado [dot] edu";
+	document.getElementById("brit_phone").innerHTML = "720-270-5003";
 }
+
+
+
+
+
+
+document.getElementById('nav_expand').onclick = function(){
+	if(toggled)
+		document.getElementById('navigation').style.display = 'none';
+	else
+		document.getElementById('navigation').style.display = 'block';
+	toggled = !toggled;
+};
